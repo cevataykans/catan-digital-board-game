234db3f603e71765288d57893e907479fc649f20
cevataykans <aykansevinc66@gmail.com>
2019-10-14 18:30:43 +0300

    Add Player, Materials, GameEngine classes.

---
+++ src/GameEngine.java
@@ -0,0 +1,15 @@
+/**
+ * GameEngine class combines logic of the game board and players to enable users to play the game through model view
+ * controller. Game Engine would constantly communicate with UIEngine to handle player/user events.
+ * @author Cevat Aykan Sevinc
+ * @version 14.10.2019
+ * Class is created to later combine all the logic.
+ */
+public class GameEngine
+{
+	// Properties
+
+	// constructors
+
+	// Functions
+}
