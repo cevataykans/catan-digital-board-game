bb53f20281cd0505ec3eb36affa1ceb991bc56e9
talhasen123 <taklamaka1919@gmail.com>
2019-10-16 22:21:59 +0300

    Edit GameEngine, Player and Card classes

--- src/Cards/Card.java
+++ src/Cards/Card.java
@@ -35,10 +35,82 @@ public abstract class Card
     abstract void playCard();
 
     /**
+     * Gets the card's name.
+     * @return card's name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the card name to the given name.
+     * @param name The name that will be set as card's name.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the card's requirements.
+     * @return card's requirements.
+     */
+    public int[] getRequirements() {
+        return requirements;
+    }
+
+    /**
+     * Sets the card requirements to the given requirements.
+     * @param requirements The requirements that will be set as card's requirements.
+     */
+    public void setRequirements(int[] requirements) {
+        this.requirements = requirements;
+    }
+
+    /**
+     * Gets the card's information.
+     * @return card's information.
+     */
+    public String getInformation() {
+        return information;
+    }
+
+    /**
+     * Sets the card information to the given information.
+     * @param information The information that will be set as card's information.
+     */
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    /**
+     * Gets the card's image.
+     * @return card's image.
+     */
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    /**
      * Sets the card image to the given image.
      * @param image The image that will be set as card's image.
      */
     public void setImage(BufferedImage image) {
         this.image = image;
+    }
+
+    /**
+     * Gets the card's playability.
+     * @return card's playability status.
+     */
+    public boolean isPlayable() {
+        return isPlayable;
+    }
+
+    /**
+     * Sets the card's playability to the given status.
+     * @param playable The status of playability that will be set as card's playable status.
+     */
+    public void setPlayable(boolean playable) {
+        isPlayable = playable;
     }
 }
--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,15 +1,79 @@
+import java.awt.*;
+
 /**
  * GameEngine class combines logic of the game board and players to enable users to play the game through model view
  * controller. Game Engine would constantly communicate with UIEngine to handle player/user events.
  * @author Cevat Aykan Sevinc
  * @version 14.10.2019
  * Class is created to later combine all the logic.
+ * Log 16.10.2019 (Talha)
+ * -----------------------
+ * Created class attributes, constructore and functions.
+ * Added function templates.
+ * Implemented some functions.
+ * -----------------------
  */
 public class GameEngine
 {
-	// Properties
+    // Attributes
+    UI ui;
+    GameBoard board;
+    Player[] players;
+    int turnNumber;
 
-	// constructors
+	// Constructor
+    public GameEngine()
+    {
+        board = new GameBoard();
+        players = new Player[4];
+        for ( int i = 0; i < 4; i++)
+        {
+            players[i] = new Player("Placeholder name", Color.BLACK);
+        }
+        turnNumber = 0;
+    }
 
 	// Functions
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
+    }
+
+    public Player getPlayer(int playerIndex)
+    {
+        return players[playerIndex];
+    }
+
+    public Player getCurrentPlayer()
+    {
+        return players[turnNumber % 4];
+    }
+
+    public int getTurn()
+    {
+        return turnNumber;
+    }
+
+    public void moveThief(int hexagonNumber)
+    {
+        // To be implemented.
+    }
+
+    public void buildRoad(boolean roadBuildingCard)
+    {
+        // To be implemented.
+    }
+
+    public void buildSettlement()
+    {
+        // To be implemented.
+    }
+
+    public void buildCity()
+    {
+        // To be implemented.
+    }
 }
