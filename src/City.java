d1c8bb0a626b4912586b9c2d34d60f77b262afb9
RafiCoktalas <rafi@coktalas.com>
2019-10-17 23:14:39 +0300

    add Structures

---
+++ src/City.java
@@ -0,0 +1,19 @@
+
+public class City extends Structure {
+
+	protected City(String name, Player owner) {
+		super(name, owner);
+		this.pointValue = 2;
+		
+		this.requirements[0] = 0;
+		this.requirements[0] = 0;
+		this.requirements[0] = 2;
+		this.requirements[0] = 0;
+		this.requirements[0] = 3;
+
+		this.information = "";
+		
+	}
+
+	
+}
---
+++ src/Settlement.java
@@ -0,0 +1,19 @@
+
+public class Settlement extends Structure {
+
+	protected Settlement(String name, Player owner) {
+		super(name, owner);
+		this.pointValue = 1;
+		
+		this.requirements[0] = 1;
+		this.requirements[0] = 1;
+		this.requirements[0] = 1;
+		this.requirements[0] = 1;
+		this.requirements[0] = 0;
+
+		this.information = "";
+		
+	}
+
+	
+}
