91748eab4126569e4f8c2ffe5d22545e823d117a
RafiCoktalas <rafi@coktalas.com>
2019-10-17 23:14:39 +0300

    add Structures

---
+++ src/Road.java
@@ -0,0 +1,19 @@
+
+public class Road extends Structure {
+
+	protected Road(String name, Player owner) {
+		super(name, owner);
+		this.pointValue = 0;
+		
+		this.requirements[0] = 1;
+		this.requirements[0] = 0;
+		this.requirements[0] = 0;
+		this.requirements[0] = 1;
+		this.requirements[0] = 0;
+
+		this.information = "";
+		
+	}
+
+	
+}
