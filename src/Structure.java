a5da30833a1aaa73969bad7a75d8bd77eb54f351
Hakan Sivük <hakansivuk@Hakan-MacBook-Pro.local>
2019-10-21 18:15:38 +0300

    Add GameBoard remaining methods, Fix small bugs

--- src/Structure.java
+++ src/Structure.java
@@ -4,11 +4,30 @@ public abstract class Structure {
 	protected String information;
 	protected int pointValue;
 	protected Player owner;
+	protected int x;
+	protected int y;
 	protected int[] requirements; //lumber, wool, grain, brick, ore
 	
 	protected Structure( String name, Player owner ) {
 		this.name = name;
 		this.owner = owner;
+		this.requirements = new int[5];
+	}
+
+	public int getType(){
+		return this.pointValue;
+	}
+
+	public Player getOwner(){
+		return this.owner;
+	}
+
+	public int getX(){
+		return x;
+	}
+
+	public int getY(){
+		return y;
 	}
 			
 }
