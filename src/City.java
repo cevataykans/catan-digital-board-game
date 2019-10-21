2a6bedc2664a3f4bc17ddcf57b118b184fca5ac6
Hakan Sivük <hakansivuk@Hakan-MacBook-Pro.local>
2019-10-21 18:15:38 +0300

    Add GameBoard remaining methods, Fix small bugs

--- src/City.java
+++ src/City.java
@@ -1,17 +1,20 @@
 
 public class City extends Structure {
 
-	protected City(String name, Player owner) {
+	protected City(String name, Player owner, int x, int y) {
 		super(name, owner);
 		this.pointValue = 2;
 		
 		this.requirements[0] = 0;
-		this.requirements[0] = 0;
-		this.requirements[0] = 2;
-		this.requirements[0] = 0;
-		this.requirements[0] = 3;
+		this.requirements[1] = 0;
+		this.requirements[2] = 2;
+		this.requirements[3] = 0;
+		this.requirements[4] = 3;
 
-		this.information = "";
+		this.x = x;
+		this.y = y;
+
+		this.information = "City info"; // It will be written
 		
 	}
 
