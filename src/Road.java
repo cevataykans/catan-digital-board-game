fe7331d51ffefaeb764f80e9527ae046322f4d8c
ynsengun <yusuf.nevzat.sengun@gmail.com>
2019-11-20 22:55:49 +0300

    bugfix on fillhexagon

--- src/Road.java
+++ src/Road.java
@@ -1,12 +1,6 @@
 
 public class Road extends Structure {
 
-	enum RotationType{
-		UPPER_LEFT_VERTICAL, UPPER_RIGHT_VERTICAL, HORIZONTAL
-	}
-
-	private Road.RotationType rotation;
-
 	protected Road(Player owner, int x, int y) {
 		super(owner);
 		this.victoryPoints = VICTORY_POINTS_FOR_ROAD;
@@ -17,14 +11,6 @@ public class Road extends Structure {
 		this.information = "Road info"; // It will be written
 
 		this.type = Type.ROAD;
-	}
-
-	public Road.RotationType getRotation(){
-		return rotation;
-	}
-
-	public void setRotation( Road.RotationType rotation ){
-		this.rotation = rotation;
 	}
 
 }
